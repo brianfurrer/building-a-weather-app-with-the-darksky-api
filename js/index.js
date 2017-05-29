43e34b97ddb904c6e9209a046158de0aadee386c
brianfurrer <brian.furrer@gmail.com>
2017-05-29 15:13:14 -0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -263,21 +263,19 @@ $('body').on('click', '#back', function() {
 // Report City & AutoFill Coords
 // =================================================
 
-function insertGoogleScript() {
-	var google_api = document.createElement('script'),
-			api_key    = 'place api key here';
-
-	// Inject the script for Google's API and reference the initGoogleAPI
-	// function as a callback.
-	google_api.src = 'https://maps.googleapis.com/maps/api/js?key='+ api_key +'&callback=initGoogleAPI&libraries=places,geometry';
-	document.body.appendChild(google_api);
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+      
+    } else { 
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
 }
 
-
-// SearchBox Method
-function initGoogleAPI() {
-	var autocomplete = new google.maps.places.SearchBox(document.querySelector("#city-search"));
-
+function showPosition(position) {
+      x.innerHTML = "Latitude: " + position.coords.latitude + 
+    "<br>Longitude: " + position.coords.longitude;
+}
 	autocomplete.addListener('places_changed', function() {
 		var place = autocomplete.getPlaces()[0];
 		document.querySelector("#latitude").value = place.geometry.location.lat();
